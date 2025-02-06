f6478bd6dead9636d161efeef9c56ffebd82b17e
EricMinana <erimincot@alu.edu.gva.es>
2025-02-06 09:28:23 +0100

    Commit inicial

---
+++ src/Entrenador.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alumne
+ */
+public class Entrenador {
+    String nom;
+    String apellido;
+    String nickname;
+    int edat;
+}
---
+++ src/Jugador.java
@@ -0,0 +1,21 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alumne
+ */
+public class Jugador {
+    String nom;
+    String apellido;
+    String nickname;
+    String rol;
+    int edat;
+    
+}
