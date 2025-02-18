40ace8869fcfa19169a45071c2884d77a1e95ad5
EricMinana <erimincot@alu.edu.gva.es>
2025-02-18 13:21:22 +0100

    Update V2

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,21 +1,7 @@
-
-import java.util.ArrayList;
-import java.util.List;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author alumne
- */
 public class Jugador {
     String nom;
-    String apellido;
+    String cognom;
     String nickname;
-    String rol;
     int edat;
-    
+    Rol rol;
 }
---
+++ src/Partido.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Partido {
+    int id;
+    Equip equipBlau;
+    Equip equipRoig;
+    Equip guanyador;
+    Date hora;
+    Jugador mvp;
+}
